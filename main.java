899dfab3b8cc566ab006f76879e61f01bed27daa
alt-OverClock <60889595+alt-OverClock@users.noreply.github.com>
2021-01-29 21:02:17 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,18 @@
 public class main {
     public static void main(String[] args){
         int array[] = {8,2,3,4,1,3,2} ;
+        int Arr[] = {14,7,3,12,9,11,6,2 };
+        int start = 0 ; end = Arr.length-1;
         InsertionSort s = new InsertionSort();
+        MegreSort m = new MergeSort();
         s.sort(array);
+        m.mergesort(Arr, start , end);
+        System.out.println("Insertion Sort");
         for(int i =0 ; i < array.length ; i++){
-            System.out.print(array[i]);
+            System.out.print(" "+array[i]);
+             System.out.println("Merge Sort");
+        for(int i =0 ; i < Arr.length ; i++){
+            System.out.print(" "+Arr[i]);
         }
     }
 }
